8b98bfcef2d23c3382ba26e5cb8a3352b679878a
Лейсан Нонская <nonskaya2003@bk.ru>
2022-10-02 19:24:49 +0300

    Delete Lesson_1_1 and add homework for 04.10

---
+++ Homework_04.10/Program.cs
@@ -0,0 +1,11 @@
+﻿int k = 0;
+double kth_term;
+double sum = 0;
+double eps = double.Parse(Console.ReadLine());
+do
+{
+    kth_term = 2 * Math.Sqrt(3) * Math.Pow(-1, k) / (Math.Pow(3, k) * (2 * k + 1));
+    sum += kth_term;
+    k++;
+} while (Math.Abs(kth_term) >= eps);
+Console.WriteLine(sum);
