dba66e7ae7d6c2f03c575f49068aedb5429a178f
Лейсан Нонская <nonskaya2003@bk.ru>
2022-10-04 07:59:21 +0300

    Removed Math.Pow

--- Homework_04.10/Program.cs
+++ Homework_04.10/Program.cs
@@ -3,10 +3,22 @@ int k = 0;
 double kth_term;
 double sum = 0;
 double eps = double.Parse(Console.ReadLine());
+int x1 = -1;
+int x2 = 1;
 do
 {
-    kth_term = 2 * Math.Sqrt(3) * Math.Pow(-1, k) / (Math.Pow(3, k) * (2 * k + 1));
+    if (k % 2 == 0)
+    {
+        x1 = 1;
+    }
+    else
+    {
+        x1 = -1;
+    }
+
+    kth_term = 2 * Math.Sqrt(3) * x1 / (x2 * (2 * k + 1));
     sum += kth_term;
     k++;
+    x2 *= 3;
 } while (Math.Abs(kth_term) >= eps);
 Console.WriteLine(sum);
